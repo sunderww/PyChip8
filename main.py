684c86afc5f058c9296338a668c4b16d0d29f3bb
Lucas Bergognon <lucas.bergognon@gmail.com>
2022-01-21 18:13:31 +0100

    Cast input to int

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def run_emulation(path: str, cpu_cycles_per_frame: int) -> None:
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        run_emulation(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else 10)
+        run_emulation(sys.argv[1], int(sys.argv[2]) if len(sys.argv) > 2 else 10)
     else:
         print("Usage: %s <rom_path> [cpu_cycles_per_frame]" % sys.argv[0])
 
